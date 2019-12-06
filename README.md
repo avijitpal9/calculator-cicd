8f658b7d0ab95c927652b193f250f6dee8d28684
Avijit Pal <avijitpal.1987@gmail.com>
2019-12-06 17:54:16 +0530

    update7

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CALCULATOR PROJECT
-## Update6
+## Update7
--- calculator.py
+++ calculator.py
@@ -12,4 +12,4 @@ def subtract(first_term, second_term):
 
 
 def multiplication(first_term, second_term):
-    return first_term + second_term
+    return first_term * second_term
