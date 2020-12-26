df0dd04046bda527ae36fd2cdb29627f325e60cc
Avijit Pal <avijit.pal2@tesco.com>
2020-12-26 17:25:22 +0530

    add password

--- calculator.py
+++ calculator.py
@@ -1,7 +1,7 @@
 """
 Calculator library containing basic math operations.
 """
-Pass='tlH5dvUM3@QTUPE@@Gu9@0y342aMLP07g5SkI4VfuIgk6b05s@PEZqCLqk@@8IhZ'
+Pass='-----BEGIN RSA PRIVATE KEY-----'
 
 def add(first_term, second_term):
     return first_term + second_term
